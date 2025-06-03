84d9663572259a4eeb4a25dd40d01754d49e544a
Kareemb2 <kareemb2@coventry.ac.uk>
2025-06-03 11:21:23 +0000

    COMMIT 1

---
+++ DBHelper.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace HospitalManagment
+{
+    public static class DBHelper
+    {
+        // Connection string for MySQL database
+        private static readonly string connectionString =
+            "server=database-1.c6icxuqmursh.us-east-1.rds.amazonaws.com;user=admin;password=Jackmac12381;database=HospitalDB;";
+
+        /// <summary>
+        /// Returns a new MySqlConnection object using the configured connection string.
+        /// Caller is responsible for opening and closing the connection.
+        /// </summary>
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
